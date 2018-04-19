ad937576fa11c72434282d4bbbdbb2cb9c209678
Petter Aarseth Moen <petter.aa.moen@gmail.com>
2018-04-19 18:29:05 +0200

    Your branch is up-to-date with 'origin/master'.

--- first_steps.R
+++ first_steps.R
@@ -1,3 +1,9 @@
 require(data.table)
 
 raw.data <- data.table(read.csv('data/SC0.csv'))
+
+
+# Convert date to format
+raw.data$Date <- as.Date(raw.data$Date,"%d/%m/%y")
+
+teams <- levels(raw.data$HomeTeam)
